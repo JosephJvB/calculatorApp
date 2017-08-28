2e91978eaea1fe520172d2bf394ef23bc92118f8
Joe <jo-van-bo@hotmail.com>
2017-08-28 13:34:01 +1200

    html structure js variables

---
+++ main.js
@@ -0,0 +1,27 @@
+var decimal = document.getElementById(".").value;
+var zero = document.getElementById("0").value;
+var equals = document.getElementById("=").value;
+var divide = document.getElementById("÷").value;
+var multiply = document.getElementById("*").value;
+var subtract = document.getElementById("-").value;
+var add = document.getElementById("+").value;
+var one = document.getElementById("1").value;
+var two = document.getElementById("2").value;
+var three = document.getElementById("3").value;
+var four = document.getElementById("4".value);
+var five = document.getElementById("5").value;
+var six = document.getElementById("6").value;
+var seven = document.getElementById("7").value;
+var eight = document.getElementById("8").value;
+var nine = document.getElementById("9").value;
+
+
+// console.log(one, add, three);
+
+function logCalc (evt) {
+  //need a function to print the calculation
+}
+
+function calcCalc (evt) {
+  //when "=" is pressed, the answer is calculated and logged
+}
