8def806520570d3c49d424d3b9829273b1adec71
Joe <jo-van-bo@hotmail.com>
2017-08-30 12:10:31 +1200

    functionality online

--- main.js
+++ main.js
@@ -1,27 +1,22 @@
-var decimal = document.getElementById(".").value;
-var zero = document.getElementById("0").value;
-var equals = document.getElementById("=").value;
-var divide = document.getElementById("÷").value;
-var multiply = document.getElementById("*").value;
-var subtract = document.getElementById("-").value;
-var add = document.getElementById("+").value;
-var one = document.getElementById("1").value;
-var two = document.getElementById("2").value;
-var three = document.getElementById("3").value;
-var four = document.getElementById("4".value);
-var five = document.getElementById("5").value;
-var six = document.getElementById("6").value;
-var seven = document.getElementById("7").value;
-var eight = document.getElementById("8").value;
-var nine = document.getElementById("9").value;
-
-
-// console.log(one, add, three);
-
-function logCalc (evt) {
-  //need a function to print the calculation
+var calcScreen = document.getElementById("display")
+function logCalc (x) {
+  calcScreen.value += x;
+  if (x==='C') {
+    calcScreen.value='';
+  }
 }
 
-function calcCalc (evt) {
-  //when "=" is pressed, the answer is calculated and logged
+function calcCalc () {
+  var x=calcScreen.value;
+  x=eval(x);
+  calcScreen.value=x;
+}
+
+
+//extra special super button :o
+function backSpace () {
+  var num = calcScreen.value;
+  var lenF = num.length-1;
+  var newNum = num.substring(0, lenF);
+  calcScreen.value = newNum;
 }
